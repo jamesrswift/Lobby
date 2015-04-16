c04a187feba5d7038ffbcf672f26c21dccedfab6
JamesxX <james.rt.swift@gmail.com>
2015-04-16 11:06:24 +0100

    Update

--- lobby_wireframespawn.lua
+++ lobby_wireframespawn.lua
@@ -7,7 +7,8 @@ if CLIENT then
 		pl = net.ReadEntity()
 		if pl and IsValid(pl) then	
 			local PrevMins, PrevMaxs = pl:GetRenderBounds()
-			VarTable[ pl ] = { true, 0 , PrevMaxs.z - PrevMins.z }
+			VarTable[ pl ] = { true, 0 , PrevMaxs.z - PrevMins.z, pl:GetMaterial() }
+			pl:SetMaterial( "models/wireframe" )
 		else
 			print( "Invalid player!", pl )
 		end
@@ -22,6 +23,7 @@ if CLIENT then
 					VarTable[pl][2] = math.Clamp( VarTable[pl][2] + ( 70 * FrameTime() ), 0 , 100 )
 					if VarTable[pl][2]  == 100 then
 						VarTable[pl][1] = false
+						pl:SetMaterial( VarTable[pl][4] )
 						return
 					end
 				end
@@ -31,27 +33,16 @@ if CLIENT then
 	
 	hook.Add( "PrePlayerDraw", "WireframeSpawn", function( pl )
 		if VarTable[pl] then
-		
 			if VarTable[pl][1] then
-				
-				local normal = Vector( 0, 0, -1 );
-				local distance = normal:Dot( pl:GetPos() + Vector( 0 , 0 , (VarTable[pl][2]/100) * VarTable[pl][3]) );
-				 
-				alreadyClipping = render.EnableClipping( true );
-				render.PushCustomClipPlane( normal, distance );
-				
+				--render.SetBlend(0)
 			end
-			
 		end
-	
 	end)
 	
 	hook.Add( "PostPlayerDraw", "WireframeSpawn", function( pl )
 		if VarTable[pl] then
 			if VarTable[pl][1] then
-				render.PopCustomClipPlane();
-				render.EnableClipping( alreadyClipping );
-				
+				--render.SetBlend(1)
 			end
 		end
 	end)
@@ -61,25 +52,48 @@ if CLIENT then
 	hook.Add( "PostDrawOpaqueRenderables", "PostDrawing", function()
 
 		for _, pl in pairs(player.GetAll()) do
+			local pos = LocalPlayer():EyePos()+LocalPlayer():EyeAngles():Forward()*-10
+			local ang = LocalPlayer():EyeAngles()
+			ang = Angle(ang.p+90,ang.y,0)
+			
 			if VarTable[pl] then
 				if VarTable[pl][1] then
 					render.ClearStencil() --Clear stencil
 					render.SetStencilEnable( true ) --Enable stencil
-					
-					render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_NEVER ) --We don't actually draw the weapon, we just want it on our stencil
-					render.SetStencilFailOperation( STENCILOPERATION_INCR ) --If we fail, do nothing
-					render.SetStencilPassOperation( STENCILOPERATION_KEEP ) --If we pass (we see it) increase the pixels value by 1
-					render.SetStencilZFailOperation( STENCILOPERATION_KEEP ) --If it's behind something, dont do anything
-
-					pl:DrawModel()
 						
-					render.SetStencilReferenceValue( 1 ) --Reference value 1
-					render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_LESSEQUAL ) --Only draw if pixel value == reference value
+						render.SetStencilWriteMask(255)
+						render.SetStencilTestMask(255)
+						render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_NEVER )
+						render.SetStencilFailOperation( STENCILOPERATION_REPLACE )
+						render.SetStencilPassOperation( STENCILOPERATION_KEEP )
+						render.SetStencilZFailOperation( STENCILOPERATION_KEEP )
+						render.SetStencilReferenceValue(2)
 						
-					render.SetMaterial( mat1 )
-					render.DrawScreenQuad() --Draw over whole screen (the pixels outside the players weapons won't be rendered anyways.
-					
-					render.ClearStencil()
+						local normal = Vector( 0, 0, -1 );
+						local distance = normal:Dot( pl:GetPos() + Vector( 0 , 0 , (VarTable[pl][2]/100) * VarTable[pl][3]) );
+						
+						render.SetBlend(0)
+						alreadyClipping = render.EnableClipping( true );
+						render.PushCustomClipPlane( normal, distance );
+						pl:DrawModel()
+						render.PopCustomClipPlane();
+						render.EnableClipping( alreadyClipping );
+						render.SetBlend(1)
+							
+						render.SetStencilReferenceValue( 2 ) --Reference value 1
+						render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_LESSEQUAL ) --Only draw if pixel value == reference value
+							
+						--[[cam.Start3D2D(pos,ang,1)
+							render.SetMaterial( mat1 )
+							render.DrawScreenQuad()
+						cam.End3D2D()--]]
+						
+						--cam.Start3D2D(pos, ang,1)
+							pl:SetMaterial(VarTable[pl][4])
+							pl:DrawModel()
+							pl:SetMaterial("models/wireframe" )
+						--cam.End3D2D()
+						
 					render.SetStencilEnable( false )
 				end
 			end
