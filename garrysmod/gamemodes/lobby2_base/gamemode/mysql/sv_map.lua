082c308e93017a95aaa4d859668986ee84b26419
James Swift <james.rt.swift@gmail.com>
2016-02-11 23:05:05 +0000

    MySQL Update for map data

--- garrysmod/gamemodes/lobby2_base/gamemode/mysql/sv_map.lua
+++ garrysmod/gamemodes/lobby2_base/gamemode/mysql/sv_map.lua
@@ -16,32 +16,46 @@ GM.MySQL.MapData = GM.MySQL.MapData or {};
 
 function GM:LoadMapInformation( MapName )
 
-	local query = self.MySQL.BuildQuery( "SELECT * FROM gm_maps WHERE map = %s LIMIT 1", MapName )
-
-	if ( query ) then
-		tmysql.query( query, function( results )
-			PrintTable( results )
-			if ( results[1] ) then
-			
-			
+	self.MySQL.SelectAll( "gm_maps", function( results )
+	
+		for row_id, row in pairs( results ) do
 		
-				hook.Run( "MapInformationLoaded", MapName )
+			local info = util.JSONToTable( row.json_data )
+			if ( info ) then
+			
+				self.MySQL.MapData[ row.map_name ] = info
+				hook.Run( "MapInformationLoaded", row.map_name )
+				
 			else
-				self:Print( "[MySQL] There was an error while loading map information!" );
+			
+				self:Print( "[MySQL] Error while loading map information for %s!", row.map_name );
+			
 			end
-		end )
-	end
+		
+		end
+	
+	end, (MapName and "WHERE map_name = %s LIMIT 1"), MapName )
 
 end
 
 function GM:GetMapData( MapName )
+
+	MapName = MapName or game.GetMap()
+	self.MySQL.MapData[ MapName ] = self.MySQL.MapData[ MapName ] or { }
 	
-	GM.MySQL.MapData[MapName] = GM.MySQL.MapData[MapName] or { }
-	return GM.MySQL.MapData[MapName]
+	return self.MySQL.MapData[ MapName ]
 
 end
 
 function GM:SaveMapData( MapName )
 
+	MapName = MapName or game.GetMap()
+	local query = self.MySQL.BuildQuery( "REPLACE INTO gm_maps ( map_name, json_data ) Values ( '%s', '%s' )", MapName, util.TableToJSON( self:GetMapData( MapName ) ) )
+
+	if ( query ) then
+		tmysql.query( query, function( results )
+	
+		end )
+	end
 
 end
